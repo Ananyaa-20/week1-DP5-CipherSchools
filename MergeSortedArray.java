4279b19f34823d085c8d084881c99ca8f69e06b2
Ananya Gupta <72341852+Ananyaa-20@users.noreply.github.com>
2023-01-20 17:04:10 +0530

    Update MergeSortedArray.java

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,1 +1,18 @@
-
+public class MergeSortedArray{
+  public void merge(int[] arr1,int m,int[] arr2, int n){
+    int i = m-1, j = n-1;
+    while(i>=0 && j>=0){
+      if(nums1[i]>=nums2[j]){
+                nums1[i+j+1]=nums1[i] ;
+                i-- ;
+                }else{
+                    nums1[i+j+1]=nums2[j];
+                    j-- ;
+                }
+        }
+        while(j>=0){
+                    nums1[j]=nums2[j];
+                    j-- ;
+                }
+    }
+}
